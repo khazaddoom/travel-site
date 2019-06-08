8e27fcccc07505c7662ac09db7208283dc09ca2d
unknown <31121986g@gmail.com>
2019-06-08 19:29:45 +0530

    gulp learning

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,28 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+watch = require('gulp-watch');
 
-gulp.task('default', function(cb) {
+
+gulp.task('default', function() {
     console.log("this is from a gulp task");
-    cb();
+
 });
 
-gulp.task('html', function(cb) {
-    console.log('html stuff here');
-    cb();
+gulp.task('html', function() {
+    console.log('something chnaged in the HTML file(s)');
+
 });
+
+gulp.task('styles', function() {
+    console.log('something chnaged in the CSS file(s)');
+
+});
+
+gulp.task('watch', function() {
+    watch(['./app/index.html'], function() {
+        gulp.task('html').call();
+    });
+
+    watch(['./app/assets/styles/**/*.css'], function() {
+        gulp.task('styles').call();
+    });
+})
