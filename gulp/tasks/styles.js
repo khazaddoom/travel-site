584cb92a8d876a556593764924488d4812ea6be8
unknown <31121986g@gmail.com>
2019-06-10 15:23:14 +0530

    gulp tasks error handling

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -12,7 +12,7 @@ gulp.task('styles', function() {
         .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
         .on('error', function(errorInfo) {
             console.log(errorInfo.reason);
-            this.emit('end')
+            this.emit('end');
         })
         .pipe(gulp.dest('./app/temp/styles')        
     );
